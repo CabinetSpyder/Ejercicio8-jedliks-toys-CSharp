a13bd05e7a33edf20c573a5212e5b406a3137caf
AndresAAV <andres.artaloytia@gmail.com>
2025-04-25 13:46:29 +0200

    Primera iteracion de la solucion

--- jedliks-toys/JedliksToys.cs
+++ jedliks-toys/JedliksToys.cs
@@ -1,22 +1,34 @@
 class RemoteControlCar
 {
+    private int _drivenMeters = 0;
+    private int _battery = 100;
+
     public static RemoteControlCar Buy()
     {
-        throw new NotImplementedException("Please implement the (static) RemoteControlCar.Buy() method");
+        return new RemoteControlCar();
     }
 
     public string DistanceDisplay()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.DistanceDisplay() method");
+        return $"Driven {_drivenMeters} meters";         
     }
 
     public string BatteryDisplay()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.BatteryDisplay() method");
+        if(_battery == 0){
+            return "Battery empty";
+        }else {
+            return $"Battery at {_battery}%"; 
+        }
+         
     }
 
     public void Drive()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.Drive() method");
+        if(_battery > 0){
+            _drivenMeters += 20;
+            _battery -= 1;
+        }
+        
     }
 }
